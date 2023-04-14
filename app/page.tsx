07620503f136bd34d091a3942185136809ea1455
ppppppsmash <pppppp.work@gmail.com>
2023-04-14 17:43:14 +0900

    add:入力欄にurlを入力するとスコアを取得する

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,36 @@
 'use client'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import { useState } from 'react'
+import { SetStateAction, useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [url, setUrl] = useState('');
   const [score, setScore] = useState(0);
 
   const getScore = async () => {
-    const res = await fetch(`https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=https://google.com&key=${process.env.NEXT_PUBLIC_API_KEY}&strategy=mobile`)
+    const res = await fetch(`https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${url}&key=${process.env.NEXT_PUBLIC_API_KEY}&strategy=mobile`)
     if (res.ok) {
       const data = await res.json()
       const lighthouse = data.lighthouseResult
       setScore(lighthouse.categories.performance.score * 100);
-  } else {
-      console.error('Failed to get data from server');
+    } else {
+        console.error('Failed to get data from server');
+    }
   }
+
+  const handleUrlChange = (event: { target: { value: SetStateAction<string> } }) => {
+    setUrl(event.target.value);
   }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        {score ? <div>Score: {score}</div> : <button onClick={getScore}>Get Score</button>}
-      </div>
-    </main>
+    <div>
+      <label htmlFor="url-input">Enter URL:</label>
+      <input className='text-gray-900' id="url-input" type="text" value={url} onChange={handleUrlChange} />
+      {score ? <div>Score: {score}</div> : <button onClick={getScore}>Get Score</button>}
+    </div>
+  </main>
   )
 }
