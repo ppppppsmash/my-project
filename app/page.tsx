215e3fcf23833d758aceb62ea06d4d3482846d34
ppppppsmash <pppppp.work@gmail.com>
2023-04-14 22:28:26 +0900

    add：API作成

--- app/page.tsx
+++ app/page.tsx
@@ -10,13 +10,12 @@ export default function Home() {
   const [score, setScore] = useState(0);
 
   const getScore = async () => {
-    const res = await fetch(`https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${url}&key=${process.env.NEXT_PUBLIC_API_KEY}&strategy=mobile`)
-    if (res.ok) {
-      const data = await res.json()
-      const lighthouse = data.lighthouseResult
-      setScore(lighthouse.categories.performance.score * 100);
-    } else {
-        console.error('Failed to get data from server');
+    const res = await fetch(`http://localhost:3000/api/pagespeedInsights?url=${url}`, {
+      cache: "no-store",
+    })
+    if(res.ok) {
+      const data =await res.json()
+      setScore(data.score)
     }
   }
 
@@ -26,13 +25,13 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <div>
+      <div>
       <label htmlFor="url-input">URL:</label>
       <input className='text-gray-900' id="url-input" type="text" value={url} onChange={handleUrlChange} />
       <div className='mt-3 text-right'>
         {score ? <div>スコア: {score}</div> : <div><button className='rounded-full bg-white text-gray-900' onClick={getScore}>スコア取得</button></div>}
       </div>
     </div>
-  </main>
+    </main>
   )
 }
