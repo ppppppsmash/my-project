baed4e5a506803053adc1e6a38c60eb524c3f912
ppppppsmash <pppppp.work@gmail.com>
2023-05-14 15:03:13 +0900

    mod:レイアウト

---
+++ client/components/Nav.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+
+interface Props {}
+
+const AdminNav: FC<Props> = (props): JSX.Element => {
+  return (
+    <nav>
+      Nav
+    </nav>
+  )
+}
+
+export default AdminNav
