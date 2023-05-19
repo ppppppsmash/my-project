25b0727cdd8b5eaed1926d77a4bf294b1881991a
ppppppsmash <pppppp.work@gmail.com>
2023-05-19 20:47:24 +0900

    add: NavにToggle機能

--- client/components/Nav/index.tsx
+++ client/components/Nav/index.tsx
@@ -1,11 +1,10 @@
 'use client'
-import { useState } from 'react'
+import { useState, useRef, useEffect, FC } from 'react'
 import Link from 'next/link'
 // https://react-icons.github.io/react-icons/icons?name=ai
 import Logo from '@/components/Logo'
 import { IconType } from 'react-icons'
 import { RiMenuFoldFill, RiMenuUnfoldFill } from 'react-icons/ri'
-import { NextPage } from 'next'
 
 interface Props {
   navItems: {
@@ -15,11 +14,39 @@ interface Props {
   }[]
 }
 
-const Nav: NextPage<Props> = ({navItems}) => {
+const NAV_OPEN_WIDTH = 'w-60'
+const NAV_CLOSE_WIDTH = 'w-12'
+const NAV_VISIBILITY = 'nav-visibility'
+
+const Nav: FC<Props> = ({navItems}): JSX.Element => {
+  const navRef = useRef<HTMLElement>(null)
   const [visible, setVisible] = useState(false)
 
+  const toggleNav = (visibility: boolean) => {
+    const { current: currentNav } = navRef
+    if(!currentNav) return
+
+    const { classList } = currentNav
+    if(visibility) {
+      // navを隠す
+      classList.remove(NAV_OPEN_WIDTH)
+      classList.add(NAV_CLOSE_WIDTH)
+    } else {
+      classList.add(NAV_OPEN_WIDTH)
+      classList.remove(NAV_CLOSE_WIDTH)
+    }
+
+  }
+
+  const updateNavState = () => {
+    toggleNav(visible)
+    const newState = !visible
+    setVisible(newState)
+    localStorage.setItem(NAV_VISIBILITY, JSON.stringify(newState))
+  }
+
   return (
-    <nav className='h-screen overflow-hidden w-60 top-0 sticky shadow-sm flex flex-col
+    <nav ref={navRef} className='h-screen overflow-hidden w-60 top-0 sticky shadow-sm flex flex-col
     justify-between transition-[width] bg-secondary text-white'>
       <div>
         <Link href='/'>
@@ -42,7 +69,7 @@ const Nav: NextPage<Props> = ({navItems}) => {
         </div>
       </div>
 
-      <button className='text-highlight-light p-3 hover:scale-[0.9] transition self-end'>
+      <button onClick={updateNavState} className='text-highlight-light p-3 hover:scale-[0.9] transition self-end'>
         {visible ? <RiMenuFoldFill size={25} /> : <RiMenuUnfoldFill size={25} />}
       </button>
     </nav>
