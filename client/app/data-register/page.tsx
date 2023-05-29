b10848e78f4b1101fb4e254fb05f641051d50e64
ppppppsmash <pppppp.work@gmail.com>
2023-05-29 21:44:41 +0900

    fix: テーブルTRごとの再取得動作

--- client/app/data-register/page.tsx
+++ client/app/data-register/page.tsx
@@ -42,8 +42,6 @@ const page: NextPage<Props> = (props): JSX.Element => {
 
   const date = new Date().toLocaleString()
 
-  
-
   const fetchPsiData = async (url: any, device: any) => {
     const res = await fetch(`${process.env.NEXT_PUBLIC_URL}pageSpeedInsights?url=${url}&strategy=${device}`, {
       cache: 'no-store'
@@ -102,7 +100,7 @@ const page: NextPage<Props> = (props): JSX.Element => {
 
 
 
-  const getScoreAgain = async () => {
+  const getScoreAgain = async (url: string) => {
     setLoading(true)
     const res = await fetchPsiData(url, selectedDevice)
 
@@ -145,7 +143,7 @@ const page: NextPage<Props> = (props): JSX.Element => {
         ? setPageList(prevState =>
           prevState.map(item => {
             if (item.url === url) {
-              return { ...item, psiData }
+              return { ...item, score: psiData.score, date }
             }
             return item
           })
@@ -153,7 +151,8 @@ const page: NextPage<Props> = (props): JSX.Element => {
         : setMobilePageList(prevState =>
           prevState.map(item => {
             if (item.url === url) {
-              return { item, ...psiData }
+              console.log(item)
+              return { ...item, score: psiData.score, date }
             }
             return item
           })
@@ -219,62 +218,21 @@ const page: NextPage<Props> = (props): JSX.Element => {
           </div>
         </div>
         <div>
-
+        {/* loading */}
         { loading && <Loading /> }
-
         {/* mobile */}
-
-        { mobileResults && selectedDevice === 'mobile' &&
-        <div>
-          <section>
-              <AnalysisTable
-                name={mobileResults.name}
-                url={mobileResults.url}
-                score={mobileResults.score}
-                date={mobileResults.date}
-                getScoreAgain={getScoreAgain}
-              />
-          </section>
-
-          <section>
+        { !loading && mobileResults && selectedDevice === 'mobile' &&
           <AnalysisTableAll
             pageList={mobilePageList}
             getScoreAgain={getScoreAgain}
           />
-          </section>
-        </div>
         }
-
-        {/* { visible &&
-          <section>
-            <AnalysisTableAll
-              pageList={pageList}
-              getScoreAgain={getScoreAgain}
-            />
-          </section>
-        } */}
-
         {/* desktop */}
-
-        { results && selectedDevice === 'desktop' &&
-        <div>
-          <section>
-            <AnalysisTable
-              name={results.name}
-              url={results.url}
-              score={results.score}
-              date={results.date}
-              getScoreAgain={getScoreAgain}
-            />
-          </section>
-
-          <section>
+        { !loading && results && selectedDevice === 'desktop' &&
           <AnalysisTableAll
             pageList={pageList}
             getScoreAgain={getScoreAgain}
           />
-          </section>
-        </div>
         }
         </div>
     </section>
--- client/components/Table/AnalysisTableAll.tsx
+++ client/components/Table/AnalysisTableAll.tsx
@@ -2,41 +2,43 @@ import { FC } from 'react'
 import { ApiResultType } from '@/type'
 
 interface Props {
-  getScoreAgain?: () => void
+  getScoreAgain: (url: any) => void
   pageList: ApiResultType[]
 }
 
 const AnalysisTableAll: FC<Props> = ({ getScoreAgain, pageList}): JSX.Element => {
   return (
-    <table className='rounded my-2 w-full mx-auto text-white border-b-2 border-gray-300'>
-      <thead className='bg-black'>
-        <tr className='text-left '>
-          <th className='px-4 py-3 font-normal text-center'>サイト名</th>
-          <th className='px-4 py-3 font-normal text-center'>URL</th>
-          <th className='px-4 py-3 font-normal text-center'>スコア</th>
-          <th className='px-4 py-3 font-normal text-center'>取得日時</th>
-          <th className='px-4 py-3 font-normal text-center'></th>
-        </tr>
-      </thead>
-      <tbody className='text-gray-900'>
-        {pageList.map((page) => (
-          <tr className='border-b hover:text-white hover:bg-gray-900' key={page.name}>
-            <td className='px-4 py-3 font-semibold text-center'>{page.name}</td>
-            <td className='px-4 py-3 text-center'>{page.url}</td>
-            <td className='px-4 py-3 text-center'>{page.score}</td>
-            <td className='px-4 py-3 text-center'>{page.date}</td>
-            <td className='px-4 py-3'>
-              <button type='button' className='transition block w-full bg-gray-900 mt-4
-              py-2 rounded text-white font-semibold mb-2 active:bg-gray-500
-              hover:scale-[0.95] active:scale-[1] hover:bg-white hover:text-gray-900'
-              onClick={getScoreAgain}
-              >再取得
-              </button>
-            </td>
+    <section>
+      <table className='rounded my-2 w-full mx-auto text-white border-b-2 border-gray-300'>
+        <thead className='bg-black'>
+          <tr className='text-left '>
+            <th className='px-4 py-3 font-normal text-center'>サイト名</th>
+            <th className='px-4 py-3 font-normal text-center'>URL</th>
+            <th className='px-4 py-3 font-normal text-center'>スコア</th>
+            <th className='px-4 py-3 font-normal text-center'>取得日時</th>
+            <th className='px-4 py-3 font-normal text-center'></th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody className='text-gray-900'>
+          {pageList.map((page) => (
+            <tr className='border-b hover:text-white hover:bg-gray-900' key={page.name}>
+              <td className='px-4 py-3 font-semibold text-center'>{page.name}</td>
+              <td className='px-4 py-3 text-center'>{page.url}</td>
+              <td className='px-4 py-3 text-center'>{page.score}</td>
+              <td className='px-4 py-3 text-center'>{page.date}</td>
+              <td className='px-4 py-3'>
+                <button type='button' className='transition block w-full bg-gray-900 mt-4
+                py-2 rounded text-white font-semibold mb-2 active:bg-gray-500
+                hover:scale-[0.95] active:scale-[1] hover:bg-white hover:text-gray-900'
+                onClick={()=>getScoreAgain(page.url)}
+                >再取得
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
   )
 }
 
