c9e27cde44e19a18a1a72e29be0dcfcc8dee6d41
ppppppsmash <pppppp.work@gmail.com>
2023-06-12 23:50:48 +0900

    fix: try catch

--- client/app/api/pageList/route.ts
+++ client/app/api/pageList/route.ts
@@ -1,24 +1,6 @@
 import { NextResponse } from "next/server"
 import mysql from 'mysql2/promise'
 
-export async function POST(request: Request, response: Response) {
-  const db = await mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    port: Number(process.env.MYSQL_PORT),
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_ROOT_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-  })
-
-  const { name, url, score } = await request.json()
-
-  const sql = 'INSERT INTO site_list_db (name, url, score) VALUES (?, ?, ?)'
-  const result = db.query(sql, [name, url, score])
-  db.end()
-
-  return NextResponse.json(result)
-}
-
 export async function GET(request: Request, response: Response) {
   const db = await mysql.createConnection({
     host: process.env.MYSQL_HOST,
@@ -28,12 +10,40 @@ export async function GET(request: Request, response: Response) {
     database: process.env.MYSQL_DATABASE,
   })
 
-  const sql = 'SELECT id, name, url, score, date FROM site_list_db'
-  const data = await db.query(sql)
+  try {
+    const sql = 'SELECT id, name, url, score, date FROM site_list_db'
+    const data = await db.query(sql)
 
-  db.end()
+    db.end()
 
-  return NextResponse.json(data)
+    return NextResponse.json(data)
+  } catch (error) {
+    console.log(`db connection failed :${error}`)
+    return NextResponse.json(error)
+  }
+}
+
+export async function POST(request: Request, response: Response) {
+  const db = await mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    port: Number(process.env.MYSQL_PORT),
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  })
+
+  try {
+    const { name, url, score } = await request.json()
+
+    const sql = 'INSERT INTO site_list_db (name, url, score) VALUES (?, ?, ?)'
+    const result = db.query(sql, [name, url, score])
+    db.end()
+
+    return NextResponse.json(result)
+  } catch (error) {
+    console.log(`db connection failed :${error}`)
+    return NextResponse.json(error)
+  }
 }
 
 export async function DELETE(request: Request, response: Response) {
@@ -45,15 +55,20 @@ export async function DELETE(request: Request, response: Response) {
     database: process.env.MYSQL_DATABASE,
   })
 
-  const { searchParams } = new URL(request.url)
-  const id = searchParams.get('id')
+  try {
+    const { searchParams } = new URL(request.url)
+    const id = searchParams.get('id')
 
-  console.log(id)
+    console.log(id)
 
-  const sql = 'DELETE FROM site_list_db WHERE id = ?'
-  const data = await db.query(sql, [id])
+    const sql = 'DELETE FROM site_list_db WHERE id = ?'
+    const result = await db.query(sql, [id])
 
-  db.end()
+    db.end()
 
-  return NextResponse.json(data)
+    return NextResponse.json(result)
+  } catch (error) {
+    console.log(`db connection failed :${error}`)
+    return NextResponse.json(error)
+  }
 }
