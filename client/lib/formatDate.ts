365dce504e87a7fd6ed609143ffdbb242f7ac488
ppppppsmash <pppppp.work@gmail.com>
2023-06-14 23:34:36 +0900

    fix: Tokyoタイムゾーン

--- client/lib/formatDate.ts
+++ client/lib/formatDate.ts
@@ -1,17 +1,27 @@
+const UTCSet = (originalDate: any) => {
+  const utcTime = originalDate.getTime()
+  const date = new Date(utcTime)
+  const offsetDiff = date.getTimezoneOffset()
+  date.setTime(utcTime + offsetDiff * 1000 * 60)
+  return date
+}
+
 export const formatDate = (timestamp: string | undefined) => {
   if(!timestamp) return
+  const TOKYO_TIMEZONE = 540
   const date = new Date(timestamp)
 
   const year = date.getFullYear()
   const month = date.getMonth() + 1
-  const day = date.getDate();
+  const day = date.getDate()
 
-  const hours = date.getHours()
+  const utcHours = UTCSet(date)
+  const hours = utcHours.getHours()
   const minutes = date.getMinutes().toString().padStart(2, '0')
   const seconds = date.getSeconds().toString().padStart(2, '0')
 
   const formattedDate = `${year}/${month}/${day}`
-  const formattedTime = `${hours - 9}:${minutes}:${seconds}`
+  const formattedTime = `${hours}:${minutes}:${seconds}`
 
   return `${formattedDate}\n${formattedTime}`
 }
