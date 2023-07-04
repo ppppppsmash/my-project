1b434ee36d19133e2e363397e5618d7779b5e40a
ppppppsmash <pppppp.work@gmail.com>
2023-07-04 21:02:32 +0900

    mod: checkboxによってpsi取得処理

--- client/components/CheckBox/Analysischeckbox.tsx
+++ client/components/CheckBox/Analysischeckbox.tsx
@@ -1,22 +1,35 @@
 import { NextPage } from 'next'
-import { FC } from 'react'
+import { FC, ChangeEvent } from 'react'
+
+import { useState } from 'react'
 
 interface Props {
-  handleChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
+  device: string
+  checkEvent?: (value: string) => void
 }
 
-const AnalysisCheckbox: NextPage<Props> = ({handleChange}): JSX.Element => {
+const AnalysisCheckbox: NextPage<Props> = ({ device, checkEvent }): JSX.Element => {
+  const [isChecked, setIsChecked] = useState(false)
+
+  const handleCheck = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value
+    const checked = event.target.checked
+    setIsChecked(checked)
+    console.log(value, checked)
+    checkEvent && checkEvent(value)
+  }
+
   return (
-    <>
       <div className="flex items-start space-x-3 py-6">
-        <input type="checkbox" className="border-gray-300 rounded h-5 w-5" />
-        <p>Desktop</p>
+        <input
+          type="checkbox"
+          className="border-gray-300 rounded h-5 w-5"
+          value={device}
+          checked={isChecked}
+          onChange={handleCheck}
+        />
+        <p>{device}</p>
       </div>
-      <div className="flex items-start space-x-3 py-6">
-        <input type="checkbox" className="border-gray-300 rounded h-5 w-5" />
-        <p>Mobile</p>
-      </div>
-    </>
   )
 }
 
