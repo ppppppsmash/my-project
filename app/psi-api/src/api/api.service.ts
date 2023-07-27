413579989c242691105e37c78e0275fcd760c46a
ppppppsmash <pppppp.work@gmail.com>
2023-07-27 22:24:34 +0900

    add: api処理作成

--- app/psi-api/src/api/api.service.ts
+++ app/psi-api/src/api/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common'
 import { site_list_db } from '../entities/api.entity'
- import { Repository } from 'typeorm'
+ import { Repository, InsertResult, UpdateResult, DeleteResult } from 'typeorm'
  import { InjectRepository } from '@nestjs/typeorm'
 
 @Injectable()
@@ -15,4 +15,19 @@ export class ApiService {
     return await this.pageRepository.find()
   }
 
+  async find(id: number): Promise<site_list_db> | null {
+    return await this.pageRepository.findOne({ where: { id } })
+  }
+
+  async create(site_list_db): Promise<InsertResult> {
+    return await this.pageRepository.insert(site_list_db)
+  }
+
+  async patch(id: number, site_list_db): Promise<UpdateResult> {
+    return await this.pageRepository.update(id, site_list_db)
+  }
+
+  async delete(id: number): Promise<DeleteResult> {
+    return await this.pageRepository.delete(id)
+  }
 }
