439167758d9b062ff231f5fe13bdd3ae064bb89c
ppppppsmash <pppppp.work@gmail.com>
2023-08-28 22:14:49 +0900

    fix: link preview処理修正

--- client/utils/getLinkPreview.ts
+++ client/utils/getLinkPreview.ts
@@ -3,12 +3,15 @@ import { urlValidate } from '@/utils/validation'
 
 export const fetchLinkPreview = async (url: string) => {
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_NEST_URL}link_preview?q=${urlValidate(url)}`, {
-      cache: 'no-store'
-    })
-    console.log(res)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_NEST_URL}link_preview?url=${urlValidate(url)}`, {
+      cache: 'no-store',
+      mode: 'cors'
+    });
+    const data = await res.json()
 
-    return res
+    console.log(data)
+
+    return data
   } catch (error) {
     console.error(error)
   }
