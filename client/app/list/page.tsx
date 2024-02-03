e942c772e3e906049518bec009b5d00a4116c8a3
ppppppsmash <pppppp.work@gmail.com>
2024-02-03 19:02:23 +0900

    feat: active gradation button

--- client/app/list/page.tsx
+++ client/app/list/page.tsx
@@ -7,7 +7,6 @@ import {
   Title
 } from '@tremor/react'
 import { zenKaku } from '@/utils/font'
-import Demo from '@/components/DemoButton'
 const queryClient = new QueryClient()
 
 const DynamicComponent = dynamicImport(() => import('@/components/Tab/TableTab'))
@@ -18,7 +17,6 @@ export default function PsiList() {
       <div>
         <Title className={`${zenKaku.className} relative md:text-xl dark:text-white dark:after:from-white after:block after:bg-bottom
           after:bg-gradient-to-r after:from-gray-700 after:via-gray-300 after:to-transparent after:h-[1px]`}>ページ一覧</Title>
-          {/* <Demo /> */}
         <Card className='mt-6 shadow-lg dark:bg-gray-950'>
           <DynamicComponent />
         </Card>
--- client/components/Button/BulkButton.tsx
+++ client/components/Button/BulkButton.tsx
@@ -9,9 +9,12 @@ interface Props {
 export default function BulkButton({ label, clickEvent }: Props) {
 
   return (
-    <div className='bg-gradient-to-r from-pink-400 to-violet-600 rounded-lg p-[2px]
-      hover:from-violet-400 hover:to-pink-600 hover:scale-[0.95] transition duration-300
-      dark:from-amber-200 dark:to-sky-400 dark:hover:from-sky-200 dark:hover:to-amber-400'>
+    <div className='from-pink-400 to-violet-600 rounded-lg p-[2px]
+    hover:from-violet-400 hover:to-pink-600 hover:scale-[0.95] transition duration-300
+    dark:from-amber-200 dark:to-sky-400 dark:hover:from-sky-200 dark:hover:to-amber-400
+    bg-[conic-gradient(from_var(--shimmer-angle),theme(colors.pink.400)_30%,theme(colors.slate.100)_40%,theme(colors.violet.400)_20%)]
+    dark:bg-[conic-gradient(from_var(--shimmer-angle),theme(colors.sky.400)_30%,theme(colors.slate.100)_40%,theme(colors.amber.400)_20%)]
+    animate-[shimmer_2.5s_linear_infinite]'>
       <Button
         className='w-[120px] rounded-lg bg-gray-950 hover:scale-[0.99] transition duration-300 hover:bg-gray-950'
         color='gray'
