0fc238ab67fee5da733fcd9b63e9703fe4279597
ppppppsmash <pppppp.work@gmail.com>
2024-03-05 22:01:11 +0900

    feat: merge

--- app/psi-app/src/auto_run/auto_run.service.ts
+++ app/psi-app/src/auto_run/auto_run.service.ts
@@ -3,6 +3,8 @@ import { Cron, Interval, SchedulerRegistry, CronExpression } from '@nestjs/sched
 import { CronJob } from 'cron'
 import axios from 'axios'
 import { v4 as uuidv4 } from 'uuid'
+const http = require('http')
+const https = require('https')
 
 @Injectable()
 export class AutoRunService {
@@ -16,8 +18,14 @@ export class AutoRunService {
 
   async getPSIAuto() {
     try {
+<<<<<<< HEAD
+      const url = `${process.env.API_BASE_URL}psi_site_list`
+      const response = await fetch(url)
+      const data = await response.json()
+=======
       const url = 'http://127.0.0.1:3000/psi_site_list'
       const { data } = await axios.get(url)
+>>>>>>> develop/liming.pei
 
       data.map(({ name, schedule, url, device, id }) => {
         this.logger.debug(
@@ -25,7 +33,7 @@ export class AutoRunService {
         )
 
         if (schedule !== '0' && schedule !== '24') {
-          this.addCronJobHours(`cron job-${uuidv4()}`, `${schedule}`, name, url, device, id)
+          this.addCronJobHours(`cron job-${uuidv4()}`, schedule, name, url, device, id)
         } else if (schedule === '24') {
           this.addCronJobDay(`cron job-${uuidv4()}`, name, url, device, id)
         } else {
@@ -36,6 +44,7 @@ export class AutoRunService {
       return data
     } catch (error) {
       console.error('Error fetching schedule data:', error.message)
+      console.error('Error Info:', error)
       return null
     }
   }
@@ -45,80 +54,113 @@ export class AutoRunService {
 
     try {
       this.logger.debug('実行する')
+<<<<<<< HEAD
+      const psiUrl = `${process.env.API_BASE_URL}psi`
+      const response = await fetch(`${psiUrl}?url=${url}&strategy=${device}`)
+      //const data = await response.json()
+=======
       const psiUrl = 'http://127.0.0.1:3000/psi'
       const { data } = await axios.get(`${psiUrl}?url=${url}&strategy=${device}`)
       console.log(data)
+>>>>>>> develop/liming.pei
 
-      const { lighthouseResult, loadingExperience } = data
-      const { categories } = lighthouseResult
-      const { performance } = categories
-      const score = performance.score * 100
-
-      const { audits: lighthouseResultAudits } = lighthouseResult
-      const { metrics: loadingExperienceAudits } = loadingExperience
-      const loadingExperienceMetrics = {
-        user_fcp: loadingExperienceAudits?.['FIRST_CONTENTFUL_PAINT_MS'],
-        user_lcp: loadingExperienceAudits?.['LARGEST_CONTENTFUL_PAINT_MS'],
-        user_fid: loadingExperienceAudits?.['FIRST_INPUT_DELAY_MS'],
-        user_cls: loadingExperienceAudits?.['CUMULATIVE_LAYOUT_SHIFT_SCORE'],
-        user_inp: loadingExperienceAudits?.['INTERACTION_TO_NEXT_PAINT'],
-        user_ttfb: loadingExperienceAudits?.['EXPERIMENTAL_TIME_TO_FIRST_BYTE']
+      const queryParams = `url=${encodeURIComponent(url)}&strategy=${encodeURIComponent(device)}`
+      const options = {
+        //hostname: process.env.API_BASE_URL_SCHEDULER,
+        hostname: '127.0.0.1',
+        port: 3000,
+        path: `/psi?${queryParams}`,
+        method: 'GET'
       }
 
-      const lighthouseResultMetrics = {
-        si: lighthouseResultAudits?.['speed-index'],
-        fcp: lighthouseResultAudits?.['first-contentful-paint'],
-        lcp: lighthouseResultAudits?.['largest-contentful-paint'],
-        tti: lighthouseResultAudits?.['interactive'],
-        tbt: lighthouseResultAudits?.['total-blocking-time'],
-        cls: lighthouseResultAudits?.['cumulative-layout-shift']
-      }
+      const req = http.request(options, (res) => {
+        let data = ''
 
-      const psiSiteMetircs = {
-        score,
-        name,
-        url,
-        lcp: lighthouseResultMetrics?.lcp?.displayValue,
-        tti: lighthouseResultMetrics?.tti?.displayValue,
-        cls: lighthouseResultMetrics?.cls?.displayValue,
-        fcp: lighthouseResultMetrics?.fcp?.displayValue,
-        tbt: lighthouseResultMetrics?.tbt?.displayValue,
-        si: lighthouseResultMetrics?.si?.displayValue,
-        user_fcp: loadingExperienceMetrics?.user_fcp?.percentile,
-        user_lcp: loadingExperienceMetrics?.user_lcp?.percentile,
-        user_fid: loadingExperienceMetrics?.user_fid?.percentile,
-        user_cls: loadingExperienceMetrics?.user_cls?.percentile,
-        user_inp: loadingExperienceMetrics?.user_inp?.percentile,
-        user_ttfb: loadingExperienceMetrics?.user_ttfb?.percentile
-      }
+        res.on('data', (chunk) => {
+          data += chunk
+        })
 
-      const psiSiteList = {
-        name,
-        url,
-        device,
-        siteMetrics: [
-          psiSiteMetircs
-        ]
-      }
+        res.on('end', async () => {
+          const { lighthouseResult, loadingExperience } = JSON.parse(data)
+          const { categories } = lighthouseResult
+          const { performance } = categories
+          const score = performance.score * 100
 
-      await axios.patch(`${process.env.API_BASE_URL}psi_site_list/${id}`, psiSiteList)
+          const { audits: lighthouseResultAudits } = lighthouseResult
+          const { metrics: loadingExperienceAudits } = loadingExperience
+          const loadingExperienceMetrics = {
+            user_fcp: loadingExperienceAudits?.['FIRST_CONTENTFUL_PAINT_MS'],
+            user_lcp: loadingExperienceAudits?.['LARGEST_CONTENTFUL_PAINT_MS'],
+            user_fid: loadingExperienceAudits?.['FIRST_INPUT_DELAY_MS'],
+            user_cls: loadingExperienceAudits?.['CUMULATIVE_LAYOUT_SHIFT_SCORE'],
+            user_inp: loadingExperienceAudits?.['INTERACTION_TO_NEXT_PAINT'],
+            user_ttfb: loadingExperienceAudits?.['EXPERIMENTAL_TIME_TO_FIRST_BYTE']
+          }
 
+          const lighthouseResultMetrics = {
+            si: lighthouseResultAudits?.['speed-index'],
+            fcp: lighthouseResultAudits?.['first-contentful-paint'],
+            lcp: lighthouseResultAudits?.['largest-contentful-paint'],
+            tti: lighthouseResultAudits?.['interactive'],
+            tbt: lighthouseResultAudits?.['total-blocking-time'],
+            cls: lighthouseResultAudits?.['cumulative-layout-shift']
+          }
+
+          const psiSiteMetircs = {
+            score,
+            name,
+            url,
+            lcp: lighthouseResultMetrics?.lcp?.displayValue,
+            tti: lighthouseResultMetrics?.tti?.displayValue,
+            cls: lighthouseResultMetrics?.cls?.displayValue,
+            fcp: lighthouseResultMetrics?.fcp?.displayValue,
+            tbt: lighthouseResultMetrics?.tbt?.displayValue,
+            si: lighthouseResultMetrics?.si?.displayValue,
+            user_fcp: loadingExperienceMetrics?.user_fcp?.percentile,
+            user_lcp: loadingExperienceMetrics?.user_lcp?.percentile,
+            user_fid: loadingExperienceMetrics?.user_fid?.percentile,
+            user_cls: loadingExperienceMetrics?.user_cls?.percentile,
+            user_inp: loadingExperienceMetrics?.user_inp?.percentile,
+            user_ttfb: loadingExperienceMetrics?.user_ttfb?.percentile
+          }
+
+          const psiSiteList = {
+            name,
+            url,
+            device,
+            siteMetrics: [
+              psiSiteMetircs
+            ]
+          }
+
+          const res = await fetch(`${process.env.API_BASE_URL}psi_site_list/${id}`, {
+            method: 'PATCH',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({...psiSiteList})
+          })
+
+          return res
+        })
+      })
+
+      req.on('error', (error) => {
+        console.error(`HTTP Request Error for ${name} ${id}:`, error)
+      })
+
+      req.end()
     } catch (error) {
-      console.error('Error fetching schedule data:', error.message)
+      console.error(`Error fetching schedule data: ${id}`, error)
     }
   }
 
   addCronJobHours(job_name: string, time: string, name: string, url: string, device: string, id: number) {
-    const job = new CronJob(`0 0-23/${time} * * *`, async () => {
+      //const job = new CronJob(`${time} * * * * *`, async () => { //テスト用
+      const job = new CronJob(`0 0-23/${time} * * *`, async () => {
       await this.getPsi(name, url,device, id)
-      this.logger.warn(`${time}時間ごとに ${job_name} を稼働する!`)
+      this.logger.warn(`${job_name} ${time}時間ごとに ${job_name} を稼働する!`)
     })
-
-    // job.addCallback(() => {
-    //   // ジョブが終了した後に自動的にジョブを削除
-    //   this.schedulerRegistry.deleteCronJob(job_name)
-    //   this.logger.warn(`${job_name} は実行済みなのでクローズ`)
-    // })
 
     this.schedulerRegistry.addCronJob(job_name, job)
     job.start()
@@ -133,11 +175,6 @@ export class AutoRunService {
       await this.getPsi(name, url,device, id)
       this.logger.warn(`毎日10:00 ${job_name} を稼働する!`)
     })
-
-    // job.addCallback(() => {
-    //   this.schedulerRegistry.deleteCronJob(job_name)
-    //   this.logger.warn(`${job_name} は実行済みなのでクローズ`)
-    // })
 
     this.schedulerRegistry.addCronJob(job_name, job)
     job.start()
